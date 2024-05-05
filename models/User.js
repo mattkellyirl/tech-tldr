82191d51c52dd10ac9ed2e1411b5fbf97985feeb
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-05 19:01:56 +0930

    added User model

---
+++ models/User.js
@@ -0,0 +1,72 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class User extends Model {
+  // Check if password is valid upon creation
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      // Encrypt password upon creation of new user
+      beforeCreate: async (newUserData) => {
+        try {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        } catch (error) {
+          throw new Error("Error hashing password: " + error.message);
+        }
+      },
+      // Encrypt password upon existing user updates password (not currently in use)
+      beforeUpdate: async (updatedUserData) => {
+        try {
+          updatedUserData.password = await bcrypt.hash(
+            updatedUserData.password,
+            10
+          );
+          return updatedUserData;
+        } catch (error) {
+          throw new Error("Error hashing updated password: " + error.message);
+        }
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
