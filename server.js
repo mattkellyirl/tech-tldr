d08dcb2404ab5415eb3a80e857df2668a2a5bd81
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-08 21:07:07 +0930

    implemented working new user and logout user routes

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// Import Dependent Files
+const routes = require("./routes");
+const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
+
+// Create instance of express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Setup session configuration
+const sess = {
+  secret: "796f7520676f74206d65",
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// Apply session middleware
+app.use(session(sess));
+
+// Create instance of handlebars with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Set Handlebars as view engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Middleware
+app.use(express.json()); // Parse incoming JSON requests
+app.use(express.urlencoded({ extended: true })); // Parse incoming URL-encoded form data
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from "public" directory
+
+// Mount application routes
+app.use(routes);
+
+// Synchronize Sequelize with the database
+sequelize.sync({ force: false }).then(() => {
+  // Start HTTP Server
+  app.listen(PORT, () => console.log("Now listening"));
+});
