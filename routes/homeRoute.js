27fe70f9e9b832b26708adc94ce3027ce063954c
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-11 18:59:10 +0930

    implemented routes and templates for login and signup pages

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -11,8 +11,9 @@ router.get("/", async (req, res) => {
       },
     });
 
-    res.render("home", {
-      title: "Tech TL;DR",
+    res.render("index", {
+      layout: "main",
+      title: "Tech TL;DR - Home",
       posts: posts.map((post) => ({
         title: post.title,
         content: post.content,
@@ -21,10 +22,40 @@ router.get("/", async (req, res) => {
       })),
     });
   } catch (err) {
-    console.error("Error Finding Posts:", err);
+    console.error("Error Rendering Home Page:", err);
     return res
       .status(500)
-      .json({ error: "Request Failed - Find All Posts", details: err });
+      .json({ error: "Request Failed - Render Home Page", details: err });
+  }
+});
+
+// Render Login Page
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login", {
+      layout: "main",
+      title: "Tech TL;DR - Login",
+    });
+  } catch (err) {
+    console.error("Error Rendering Login Page:", err);
+    return res
+      .status(500)
+      .json({ error: "Request Failed - Render Login Page", details: err });
+  }
+});
+
+// Render Signup Page
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup", {
+      layout: "main",
+      title: "Tech TL;DR - Signup",
+    });
+  } catch (err) {
+    console.error("Error Rendering Signup Page:", err);
+    return res
+      .status(500)
+      .json({ error: "Request Failed - Render Signup Page", details: err });
   }
 });
 
