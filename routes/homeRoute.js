6a09b89672a0d32e5b303badedde644ddf631bcf
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-12 11:17:57 +0930

    implemented routes and templates for dashboard and post/:id

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -19,6 +19,7 @@ router.get("/", async (req, res) => {
         content: post.content,
         created_at: dayjs(post.created_at).format("dddd, MMMM DD YYYY"),
         username: post.user.username,
+        post_id: post.id,
       })),
     });
   } catch (err) {
@@ -56,6 +57,47 @@ router.get("/signup", async (req, res) => {
     return res
       .status(500)
       .json({ error: "Request Failed - Render Signup Page", details: err });
+  }
+});
+
+// Render Dashboard Page
+router.get("/dashboard", async (req, res) => {
+  try {
+    res.render("dashboard", {
+      layout: "main",
+      title: "Tech TL;DR - Dashboard",
+    });
+  } catch (err) {
+    console.error("Error Rendering Dashboard Page:", err);
+    return res
+      .status(500)
+      .json({ error: "Request Failed - Render Dashboard Page", details: err });
+  }
+});
+
+// Render TL;DR Post
+router.get("/post/:id", async (req, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id, {
+      include: User,
+    });
+
+    res.render("post", {
+      layout: "main",
+      title: "Tech TL;DR - Post",
+      post: {
+        title: post.title,
+        content: post.content,
+        created_at: dayjs(post.created_at).format("dddd, MMMM DD YYYY"),
+        username: post.user.username,
+        post_id: post.id,
+      },
+    });
+  } catch (err) {
+    console.error("Error Rendering Post:", err);
+    return res
+      .status(500)
+      .json({ error: "Request Failed - Render Post", details: err });
   }
 });
 
